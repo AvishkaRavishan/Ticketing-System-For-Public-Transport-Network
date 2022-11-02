1cd10995bc69d01ce16412c87c08bfa61f446ca4
HimankaM <87530254+HimankaM@users.noreply.github.com>
2022-11-02 18:12:08 +0530

    interface

---
+++ lib/pallete.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const TextStyle kBodyText =
+    TextStyle(fontSize: 22, color: Colors.white, height: 1.5);
+
+const Color kWhite = Colors.white;
+const Color kBlue = Color.fromARGB(255, 139, 96, 31);
---
+++ lib/screens/login-screen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import '../pallete.dart';
+import '../widgets/widgets.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        BackgroundImage(
+          image: 'assets/images/bus.jpg',
+        ),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Column(
+            children: [
+              Flexible(
+                child: Center(
+                  child: Text(
+                    '     Welcome to,\n\nSLTB TICKETING',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 45,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  TextInputField(
+                    icon: FontAwesomeIcons.envelope,
+                    hint: 'Email',
+                    inputType: TextInputType.emailAddress,
+                    inputAction: TextInputAction.next,
+                  ),
+                  PasswordInput(
+                    icon: FontAwesomeIcons.lock,
+                    hint: 'Password',
+                    inputAction: TextInputAction.done,
+                  ),
+                  GestureDetector(
+                    onTap: () => Navigator.pushNamed(context, 'ForgotPassword'),
+                    child: Text(
+                      'Forgot Password',
+                      style: kBodyText,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  RoundedButton(
+                    buttonName: 'Login',
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                ],
+              ),
+              GestureDetector(
+                onTap: () => Navigator.pushNamed(context, 'CreateNewAccount'),
+                child: Container(
+                  child: Text(
+                    'Create New Account',
+                    style: kBodyText,
+                  ),
+                  decoration: BoxDecoration(
+                      border:
+                          Border(bottom: BorderSide(width: 1, color: kWhite))),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
